ed294218424827d231541f5450081a5cdb906fa7
Rose Rodrigues <rorha04@gmail.com>
2026-06-18 00:53:58 -0500

    typos schmypos

--- ocarina-os/ci-build.sh
+++ ocarina-os/ci-build.sh
@@ -5,15 +5,15 @@ set -e
 
 echo "Cloning Poky/BitBake"
 
-[ -d poky ] git clone -b scarthgap https://git.yoctoproject.org/poky
+[ -d poky ] || git clone -b scarthgap https://git.yoctoproject.org/poky
 
 echo "Cloning meta-raspberrypi"
 
-[ -d meta-raspberrypi ] git clone -b scarthgap https://github.com/agherzan/meta-raspberrypi
+[ -d meta-raspberrypi ] || git clone -b scarthgap https://github.com/agherzan/meta-raspberrypi
 
 echo "Cloning meta-openembedded"
 
-[ -d meta-openembedded ] git clone -b scarthgap https://github.com/openembedded/meta-openembedded.git
+[ -d meta-openembedded ] || git clone -b scarthgap https://github.com/openembedded/meta-openembedded.git
 
 # Set up source
 
